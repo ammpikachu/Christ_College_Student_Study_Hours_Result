8cdad7a41b602cb6714e1e85d832e098fc347f2f
ammpikachu <amrutharoshan5@gmail.com>
2026-09-17 11:50:28 +0530

    Update model path to include version number

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import joblib
 from pathlib import Path
 
-model_path = Path(__file__).parent / "student_pass_fail_modell.pkl"
+model_path = Path(__file__).parent / "student_pass_fail_modell(1).pkl"
 model = joblib.load(model_path)
 
 st.title("Student Pass Predictor")
